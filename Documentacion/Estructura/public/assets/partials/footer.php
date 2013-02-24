d319f7b7912cd4a8592d2592cfc7bfee3b46f3ef
unknown <natt.81@gmail.com>
2013-02-24 21:23:10 +0100

    Se incluye el manual de PHP que estoy haciendo

--- Documentacion/Estructura/public/assets/partials/footer.php
+++
@@ -1,1 +0,0 @@
-<h4>Full Moon © 2010 All rights reserved. Powered by: <span class="link"><a href="http://themeforest.net/user/agurghis?ref=agurghis" class="footer_link">Alex Gurghis</a></span></h4>
--- Documentacion/Estructura/public/assets/partials/twitter.php
+++
@@ -1,3 +0,0 @@
-<script src="Scripts/twitter.js" type="text/javascript"></script>
-<script src="http://www.twitter.com/statuses/user_timeline/agurghis.json?callback=twitterCallback1&amp;count=1" type="text/javascript"></script>
-<script src="http://www.twitter.com/statuses/user_timeline/agurghis.json?callback=twitterCallback2&amp;count=1" type="text/javascript"></script>
